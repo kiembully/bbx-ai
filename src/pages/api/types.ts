a156c332ab0d2a434607380901c9700a2da8bba4
kiembully <kimklintonvale@gmail.com>
2025-05-15 16:13:14 +0800

    update types

--- src/pages/api/types.ts
+++ src/pages/api/types.ts
@@ -1,5 +1,5 @@
 
-type Bey = {
+export type Bey = {
   name: string;
   type: 'Attack' | 'Defense' | 'Stamina' | string;
   attack: number;
@@ -10,24 +10,13 @@ type Bey = {
   spin: 'Right' | 'Left' | string;
 };
 
-type Results = {
+export type Results = {
   bey1: number;
   bey2: number;
   draws: number;
 };
 
-type OutcomeDetails = {
+export type OutcomeDetails = {
   bey1: { KO: number; BURST: number; OUTSPIN: number };
   bey2: { KO: number; BURST: number; OUTSPIN: number };
 };
-
-export interface RunSymmetricBattleProps {
-  data: {
-    attacker: Bey,
-    defender: Bey,
-    results: Results,
-    details: OutcomeDetails,
-    attackerKey: keyof Results & keyof OutcomeDetails,
-    defenderKey: keyof Results & keyof OutcomeDetails
-  }
-}
